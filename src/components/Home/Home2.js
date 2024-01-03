2a44f9c8ca98094ae713dc7d8efbff9bff4e2f53
shafazhafirag <143633377+shafazhafirag@users.noreply.github.com>
2024-01-03 12:48:29 +0700

    Update Home2.js menambahkan tombol button

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col,  Button } from "react-bootstrap";
 import myImg from "../../Assets/ss.png";
 import Skill from "./Skill";
 import Tilt from "react-parallax-tilt";
